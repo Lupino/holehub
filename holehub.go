caf3382c09b79ff86bf49a66f3b1289ec9ef4515
Lupino <lmjubuntu@gmail.com>
2015-06-13 10:34:19 +0800

    Add permissions

--- holehub.go
+++ holehub.go
@@ -3,6 +3,7 @@ package main
 import (
   "github.com/codegangsta/negroni"
   "github.com/tylerb/graceful"
+  "github.com/xyproto/permissions2"
   "net/http"
   "fmt"
   "time"
@@ -10,12 +11,27 @@ import (
 
 func main() {
   mux := http.NewServeMux()
+
+  // New permissions middleware
+  perm := permissions.New()
+
+  // Get the userstate, used in the handlers below
+  // userstate := perm.UserState()
+
   mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
     fmt.Fprintf(w, "Hello HoleHub.")
   })
 
+   // Custom handler for when permissions are denied
+   perm.SetDenyFunction(func(w http.ResponseWriter, req *http.Request) {
+     http.Error(w, "Permission denied!", http.StatusForbidden)
+   })
+
   n := negroni.Classic()
+
+  n.Use(perm)
   n.UseHandler(mux)
+
   //n.Run(":3000")
   graceful.Run(":3000", 10*time.Second, n)
 }
